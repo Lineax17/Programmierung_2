acf4510bef56ce3a4f9dd6114cab787b89dec28b
Lineax17 <philipp.hartmann04@proton.me>
2024-06-21 16:21:49 +0200

    Abgabe 13 angefangen

---
+++ Abgabe_13/src/thd/game/level/Level9.java
@@ -0,0 +1,93 @@
+package thd.game.level;
+
+public class Level9 extends Level{
+    /**
+     * Initializes the level.
+     */
+    public Level9() {
+        super.world = """
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                WW            WWWWW\s
+                 WWWWW                WW            WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW      G                       WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                             RWWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWWW                             WWWWW\s
+                 WWWWWWW                           WWWWWW\s
+                 WWWWWWWW                          WWWWWW\s
+                 WWWWWWWWW                          WWWWW\s
+                 WWWWWWWWWL                         WWWWW\s
+                 WWWWWWWWWW                        WWWWWW\s
+                 WWWWWWWWWW                        WWWWWW\s
+                 WWWWWWWWW          G               WWWWW\s
+                 WWWWWWWW                           WWWWW\s
+                 WWWWWWWW                          WWWWWW\s
+                 WWWWWWWWW                        WWWWWWW\s
+                 WWWWWWWWWW                       WWWWWWW\s
+                 WWWWWWWWWWW                       WWWWWW\s
+                 WWWWWWWWWWW         222            WWWWW\s
+                 WWWWWWWWWW           22             WWWW\s
+                 WWWWWWWWW                            WWW\s
+                 WWWWWWWWWL                           WWW\s
+                 WWWWWWWWW                           WWWW\s
+                 WWWWWWWW                           WWWWW\s
+                 WWWWWWW                            WWWWW\s
+                 WWWWWW                             WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                  F           WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW      WW                      WWWWW\s
+                 WWWWW      WW                      WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 WWWWW                              WWWWW\s
+                 """;
+        number = 6;
+        name = "Level 6";
+        worldOffsetLines = world.split("\\R").length - 23;
+        worldOffsetColumns = 0;
+    }
+}
